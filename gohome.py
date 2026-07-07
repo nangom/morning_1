91fc7c4334f6bd8f1c9973783176d930c5031ecb
nangom <gudwlsdl94@gmail.com>
2026-07-07 09:23:35 +0900

    주제 뭐하징

---
+++ gohome.py
@@ -0,0 +1,18 @@
+
+while True:
+    k = input('주제 뭐 할랭: ')
+    if k == '1':
+        print('투자 자산관리 서비스')
+        continue
+    elif k == '2':
+        print('디지털뱅킹서비스')
+        continue
+    elif k == '3':
+        print('보험 라이프케어 서비스')
+        continue
+    elif k == '4':
+        print('업무혁신서비스')
+        continue
+    else:
+        print('잠이나 쳐 자라')
+        break
